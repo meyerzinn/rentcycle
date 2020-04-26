20c9057b10ae86c776ec8c7055ddd3155d5acec4
Meyer Zinn <meyerzinn@gmail.com>
2020-04-25 20:48:55 -0500

    Add firestore

---
+++ lib/keys.dart
@@ -0,0 +1,8 @@
+import 'package:firebase_core/firebase_core.dart';
+
+final AppFirebaseOptions = FirebaseOptions(
+    googleAppID: "",
+    apiKey: "",
+    databaseURL: "",
+    projectID: ""
+);
--- lib/main.dart
+++ lib/main.dart
@@ -3,22 +3,38 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:rentcycle/create_request_title_page.dart';
 import 'view_requests.dart';
 import 'util.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() => runApp(MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final FirebaseApp app = await FirebaseApp.configure(
+    name: 'test',
+    options: FirebaseOptions(
+      googleAppID: "",
+      apiKey: "",
+      projectID: "rentcycle",
+      databaseURL: "https://rentcycle.firebaseio.com"
+    ),
+  );
+  runApp(MyApp());
+}
 
 ThemeData buildTheme(BuildContext context) {
-  final textTheme = Theme.of(context).textTheme;
+  final textTheme = Theme
+      .of(context)
+      .textTheme;
   return ThemeData(
     scaffoldBackgroundColor: Colors.white,
     appBarTheme: AppBarTheme(
       color: Colors.white,
       brightness: Brightness.light,
       textTheme: textTheme.copyWith(
-        title: GoogleFonts.roboto(
-          fontWeight: FontWeight.bold,
-          fontSize: 26,
-          textStyle: textTheme.title,
-        )
+          title: GoogleFonts.roboto(
+            fontWeight: FontWeight.bold,
+            fontSize: 26,
+            textStyle: textTheme.title,
+          )
       ),
       elevation: 0,
       iconTheme: IconTheme.of(context).copyWith(color: Colors.black),
@@ -26,24 +42,24 @@ ThemeData buildTheme(BuildContext context) {
     highlightColor: Colors.transparent,
     textTheme: textTheme.copyWith(
       // section/card titles
-      title: GoogleFonts.openSans(
-        fontWeight: FontWeight.w700,
-        fontStyle: FontStyle.normal,
-        fontSize: 16,
-        textStyle: textTheme.title,
-      ),
-      subtitle: GoogleFonts.openSans(
-        fontStyle: FontStyle.normal,
-        fontSize: 12,
-        textStyle: textTheme.subtitle
-      ),
-      display1: GoogleFonts.roboto(
-        fontStyle: FontStyle.normal,
-        fontWeight: FontWeight.bold,
-        color: ACCENT_COLOR,
-        textStyle: textTheme.display1,
-        fontSize: 35
-      )
+        title: GoogleFonts.openSans(
+          fontWeight: FontWeight.w700,
+          fontStyle: FontStyle.normal,
+          fontSize: 16,
+          textStyle: textTheme.title,
+        ),
+        subtitle: GoogleFonts.openSans(
+            fontStyle: FontStyle.normal,
+            fontSize: 12,
+            textStyle: textTheme.subtitle
+        ),
+        display1: GoogleFonts.roboto(
+            fontStyle: FontStyle.normal,
+            fontWeight: FontWeight.bold,
+            color: ACCENT_COLOR,
+            textStyle: textTheme.display1,
+            fontSize: 35
+        )
     ),
   );
 }
